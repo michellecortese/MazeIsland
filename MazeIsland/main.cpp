c230a314d444ac2166578e8ba47d8d7c9a9c4c62
Michelle Cortese <mcortese_@192.168.1.3>
2013-10-01 08:59:53 -0400

    Initial Commit

---
+++ MazeIsland/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MazeIsland
+//
+//  Created by Michelle Cortese on 2013-10-01.
+//  Copyright (c) 2013 Michelle Cortese. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
